a301fdfb964d83f1dd3ed3a30425dc5a7d9812cb
hunterkoth <hunterkoth04@gmail.com>
2023-07-10 13:19:30 -0700

    fix

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -7,7 +7,7 @@ export const getNote = /* GraphQL */ `
       id
       name
       description
-      image
+      file
       createdAt
       updatedAt
       __typename
@@ -25,7 +25,7 @@ export const listNotes = /* GraphQL */ `
         id
         name
         description
-        image
+        file
         createdAt
         updatedAt
         __typename
